98563e0260f25b9c7a2985c2aa89709d65d536de
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2014-10-30 21:28:32 +0100

    Working solution for binary.

--- meetup4/exercism-assignments/1_easy/binary/binary.exs
+++ meetup4/exercism-assignments/1_easy/binary/binary.exs
@@ -6,7 +6,16 @@ defmodule Binary do
   """
   @spec to_decimal(String.t) :: non_neg_integer
   def to_decimal(string) do
-
+    string
+      |> String.reverse
+      |> String.codepoints
+      |> to_decimal
   end
 
+  defp to_decimal(codepoints) do
+    add_to_sum = fn (bin, {sum, exp}) -> {sum + (exp * parse_bin(bin)), exp * 2} end
+    Enum.reduce(codepoints, {0, 1}, add_to_sum) |> elem 0
+  end
+
+  defp parse_bin(bin), do: Integer.parse(bin) |> elem(0)
 end
