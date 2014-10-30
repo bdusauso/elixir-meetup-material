14faa77ad640e9c2844244e10c7f4769ae0c8e9a
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2014-10-30 20:03:39 +0100

    Make it work.

--- meetup4/exercism-assignments/1_easy/gigasecond/gigasecond.exs
+++ meetup4/exercism-assignments/1_easy/gigasecond/gigasecond.exs
@@ -1,13 +1,29 @@
 defmodule Gigasecond do
 
+  @seconds_count 1_000_000_000
+
 	@doc """
 	Calculate a date one billion seconds after an input date.
 	"""
 	@spec from({pos_integer, pos_integer, pos_integer}) :: :calendar.date
 
-	def from(date = {_year, _month, _day}) do
-
+	def from(date) do
+    {date, {0, 0, 0}}
+      |> convert_to_seconds
+      |> add_seconds_count
+      |> convert_to_date
 	end
 
+  defp convert_to_seconds(datetime) do
+    :calendar.datetime_to_gregorian_seconds(datetime)
+  end
+
+  defp add_seconds_count(seconds) do
+    seconds + @seconds_count
+  end
+
+  defp convert_to_date(seconds) do
+    :calendar.gregorian_seconds_to_datetime(seconds) |> elem(0)
+  end
 
 end
