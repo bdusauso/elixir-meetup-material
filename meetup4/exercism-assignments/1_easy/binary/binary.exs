cc9ecb50a95ea0f5914e4482cc1e499aef719ce4
Bruno Dusausoy <bruno.dusausoy@gmail.com>
2014-10-30 21:56:16 +0100

    Fixed function defined twice.

--- meetup4/exercism-assignments/1_easy/binary/binary.exs
+++ meetup4/exercism-assignments/1_easy/binary/binary.exs
@@ -9,10 +9,10 @@ defmodule Binary do
     string
       |> String.reverse
       |> String.codepoints
-      |> to_decimal
+      |> _to_decimal
   end
 
-  defp to_decimal(codepoints) do
+  defp _to_decimal(codepoints) do
     add_to_sum = fn (bin, {sum, exp}) -> {sum + (exp * parse_bin(bin)), exp * 2} end
     Enum.reduce(codepoints, {0, 1}, add_to_sum) |> elem 0
   end
